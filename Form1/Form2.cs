b18a933e74b8f24bb759a7880a55c753bdf769fc
Shehryar Kamran <kool_dude1996@live.com>
2018-07-02 21:19:44 +0500

    Initial commit

---
+++ Form1/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 ob = new Form1();
+            // ob.Show();
+            ob.Focus();
+            this.Hide();
+        }
+        public void ftn(string x)
+        {
+            label1.Text = x;
+        }
+    }
+}
